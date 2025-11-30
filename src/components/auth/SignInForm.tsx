1f62f31b3fb48f1ea8daf97d1bf6609beccb8a6a
Pedro Stabile Neto <stabilepedro010403@gmail.com>
2025-11-30 13:25:25 -0300

    validação de login

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -25,7 +25,7 @@ export default function SignInForm() {
       setLoading(false);
     } catch (err) {
       setLoading(false);
-      showErrorToast("Erro ao fazer login. Verifique suas credenciais e tente novamente.");
+      showErrorToast(err?.response?.data?.message.toString() || "Erro ao fazer login. Tente novamente.");
     }
   };
 
@@ -34,44 +34,24 @@ export default function SignInForm() {
     <div className="mb-5">
         <div>
           <div className="mb-5 sm:mb-8 flex flex-col sm:flex-row items-center sm:items-start text-center sm:text-left gap-4">
-  {/* Logo */}
-  <Link to="/" className="flex-shrink-0 flex items-center justify-center sm:justify-start">
-    <img src="/images/logo/PNG PRETO.png" alt="Logo" width={80} />
-  </Link>
+            {/* Logo */}
+            <Link to="/" className="flex-shrink-0 flex items-center justify-center sm:justify-start">
+              <img src="/images/logo/PNG PRETO.png" alt="Logo" width={80} />
+            </Link>
 
-  {/* Textos */}
-  <div>
-    <h1 className="mt-3 sm:mt-0 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
-      Faça o login
-    </h1>
+            {/* Textos */}
+            <div>
+              <h1 className="mt-3 sm:mt-0 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
+                Faça o login
+              </h1>
 
-    <p className="mt-3 text-sm text-gray-500 dark:text-gray-400 max-w-xs">
-      Insira o seu email e senha para acessar sua conta.
-    </p>
-  </div>
-</div>
-
-
+              <p className="mt-3 text-sm text-gray-500 dark:text-gray-400 max-w-xs">
+                Insira o seu email e senha para acessar sua conta.
+              </p>
+            </div>
+          </div>
 
           <div>
-            {/* <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 sm:gap-5">
-              <button className="inline-flex items-center justify-center gap-3 py-3 text-sm font-normal text-gray-700 transition-colors bg-gray-100 rounded-lg px-7 hover:bg-gray-200 hover:text-gray-800 dark:bg-white/5 dark:text-white/90 dark:hover:bg-white/10">
-                Sign in with Google
-              </button>
-              <button className="inline-flex items-center justify-center gap-3 py-3 text-sm font-normal text-gray-700 transition-colors bg-gray-100 rounded-lg px-7 hover:bg-gray-200 hover:text-gray-800 dark:bg-white/5 dark:text-white/90 dark:hover:bg-white/10">
-                Sign in with X
-              </button>
-            </div>
-            <div className="relative py-3 sm:py-5">
-              <div className="absolute inset-0 flex items-center">
-                <div className="w-full border-t border-gray-200 dark:border-gray-800"></div>
-              </div>
-              <div className="relative flex justify-center text-sm">
-                <span className="p-2 text-gray-400 bg-white dark:bg-gray-900 sm:px-5 sm:py-2">
-                  Or
-                </span>
-              </div>
-            </div> */}
             <form onSubmit={handleSubmit}>
               <div className="space-y-6">
                 <div>
@@ -115,15 +95,6 @@ export default function SignInForm() {
                   </div>
                 </div>
                 <div className="flex items-center justify-between">
-                  {/* <div className="flex items-center gap-3">
-                    <Checkbox
-                      checked={isChecked}
-                      onChange={(val: boolean) => setIsChecked(val)}
-                    />
-                    <span className="block font-normal text-gray-700 text-theme-sm dark:text-gray-400">
-                      Keep me logged in
-                    </span>
-                  </div> */}
                   <Link
                     to="/reset-password"
                     className="text-sm text-brand-500 hover:text-brand-600 dark:text-brand-400"
