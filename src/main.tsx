bd8219b115fd84ad77ee4d597a80ab7f88447082
Pedro Stabile Neto <stabilepedro010403@gmail.com>
2026-01-14 20:20:03 -0300

    desespero

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,8 @@ import "swiper/swiper-bundle.css";
 import "flatpickr/dist/flatpickr.css";
 import App from "./App.tsx";
 import { AppWrapper } from "./components/common/PageMeta.tsx";
-import { ThemeProvider } from "./context/ThemeContext.tsx";
+import { ThemeProvider } from "@/context/ThemeContext";
+
 import { AuthProvider } from "@/context/AuthContext";
 
 
