2c87fd7eab62d4c1c86e7c1fdf6fe3ba2b904a2c
Pedro Stabile Neto <stabilepedro010403@gmail.com>
2026-01-15 19:33:28 -0300

    teste deploy!

--- src/pages/AuthPages/SignIn.tsx
+++ src/pages/AuthPages/SignIn.tsx
@@ -8,6 +8,7 @@ export default function SignIn() {
         title="Aliance | ERP para igrejas"
         description="Página de login do sistema Aliance ERP."
       />
+      <h2>ISSO É UM TESTE DE DEPLOY</h2>
 
       <div className="min-h-screen flex items-center justify-center" style={{ backgroundImage: `url("./images/backgrounds/fundo.jpg")`, backgroundSize: "cover" }}>
         <div className="bg-white p-10 rounded-2xl shadow-2xl w-full max-w-2xl border border-gray-200">
