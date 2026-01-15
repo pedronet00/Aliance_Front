c3da2f18149a0fe0ed54baaefa224232810a1933
Pedro Stabile Neto <stabilepedro010403@gmail.com>
2026-01-15 19:52:06 -0300

    teste final absoluto

--- src/pages/AuthPages/SignIn.tsx
+++ src/pages/AuthPages/SignIn.tsx
@@ -4,17 +4,7 @@ import SignInForm from "../../components/auth/SignInForm";
 export default function SignIn() {
   return (
     <>
-      <PageMeta
-        title="Aliance | ERP para igrejas"
-        description="Página de login do sistema Aliance ERP."
-      />
-      <h2>ISSO É UM TESTE DE DEPLOY</h2> 
-
-      <div className="min-h-screen flex items-center justify-center" style={{ backgroundImage: `url("./images/backgrounds/fundo.jpg")`, backgroundSize: "cover" }}>
-        <div className="bg-white p-10 rounded-2xl shadow-2xl w-full max-w-2xl border border-gray-200">
-          <SignInForm />
-        </div>
-      </div>
+      <p>teste final absoluto</p>
     </>
   );
 }
